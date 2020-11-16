5b1f124b6cdbf0ee64bbd2692ed9ba4bc4873244
Evan Barter <evan@evanbarter.me>
2020-11-16 12:13:19 -0800

    Make list sub-components more useful

--- stubs/resources/views/components/list/accordion-item.blade.php
+++ stubs/resources/views/components/list/accordion-item.blade.php
@@ -1,12 +1,12 @@
-@props(['handle' => '', 'key' => ''])
+@props(['handle', 'key' => '', 'handleClass' => '', 'contentClass' => ''])
 
 @php
     $attributes['id'] = !empty($key) ? $key : uniqid('list-collapsible-');
 @endphp
 
 <div {{ $attributes->merge(['class' => 'hui-list__item--accordion']) }}>
-    <div @click="selected = '{{ $key }}'" class="hui-list__item-handle" role="button">{{ $handle }}</div>
-    <div class="hui-list__item-content" x-show="selected === '{{ $key }}'">
+    <div @click="selected = '{{ $key }}'" class="hui-list__item-handle{{ !empty($handleClass) ? ' '.$handleClass : '' }}" role="button">{{ $handle }}</div>
+    <div class="hui-list__item-content{{ !empty($contentClass) ? ' '.$contentClass : '' }}" x-show="selected === '{{ $key }}'">
         {{ $slot }}
     </div>
 </div>
--- stubs/resources/views/components/list/collapsible-item.blade.php
+++ stubs/resources/views/components/list/collapsible-item.blade.php
@@ -1,12 +1,12 @@
-@props(['handle' => '', 'key' => ''])
+@props(['handle', 'key' => '', 'handleClass' => '', 'contentClass' => ''])
 
 @php
     $attributes['id'] = !empty($key) ? $key : uniqid('list-collapsible-');
 @endphp
 
 <div id="{{ $key }}" {{ $attributes->merge(['class' => 'hui-list__item--collapsible']) }}>
-    <div @click="selected['{{ $key }}'] = !selected['{{ $key }}']" class="hui-list__item-handle" role="button">{{ $handle }}</div>
-    <div class="hui-list__item-content" x-show="selected['{{ $key }}'] === true">
+    <div @click="selected['{{ $key }}'] = !selected['{{ $key }}']" class="hui-list__item-handle{{ !empty($handleClass) ? ' '.$handleClass : '' }}" role="button">{{ $handle }}</div>
+    <div class="hui-list__item-content{{ !empty($contentClass) ? ' '.$contentClass : '' }}" x-show="selected['{{ $key }}'] === true">
         {{ $slot }}
     </div>
 </div>
