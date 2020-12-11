df23228e0331106dd8b401ec4cf48325277d6b89
Evan Barter <evan@evanbarter.me>
2020-12-11 10:42:39 -0800

    Make the modal classes a bit more flexible

--- stubs/resources/views/components/modal.blade.php
+++ stubs/resources/views/components/modal.blade.php
@@ -1,11 +1,11 @@
-@props(['handle' => '', 'backdropClass' => 'absolute bg-black bg-opacity-25'])
+@props(['handle' => '', 'backdropClass' => 'absolute inset-0 bg-black bg-opacity-25', 'contentClass' => 'flex items-end sm:items-center justify-center min-h-screen text-center'])
 
 <div x-data="{ open: false }" @keydown.window.escape="open = false" @click.away="open = false" class="hui-modal">
     <div @click="open = !open" role="button" class="hui-modal__handle inline-block">{{ $handle }}</div>
     <div x-show="open" class="fixed z-10 inset-0" x-cloak>
-        <div class="flex items-end sm:items-center justify-center min-h-screen text-center">
+        <div class="{{ $contentClass }}">
             <div @click="open = false" class="fixed inset-0">
-                <div class="absolute inset-0 {{ $backdropClass }}"></div>
+                <div class="{{ $backdropClass }}"></div>
             </div>
             <div {{ $attributes->merge(['class' => 'hui-modal__content align-middle z-20']) }}>
                 {{ $slot }}
